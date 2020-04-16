f44109299bdee03f9852e8a37b1e44fad4a27fee
Nate Berkopec <nate.berkopec@gmail.com>
2020-04-16 14:43:20 +0900

    Require Terraform 0.12.
    + upgrade AWS provider block

--- main.tf
+++ main.tf
@@ -1,9 +1,12 @@
-provider "aws" {
-    region = "${var.AWS_REGION}"
-    access_key = "${var.AWS_ACCESS_KEY}"
-    secret_key = "${var.AWS_SECRET_KEY}"
-    }
+terraform {
+  required_version = ">= 0.12"
+}
 
+provider "aws" {
+    region = var.aws_region
+    access_key = var.AWS_ACCESS_KEY
+    secret_key = var.AWS_SECRET_KEY
+}
 
 data "template_file" "user_data" {
     template = "/scripts/user_data.ps1"
