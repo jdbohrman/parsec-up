88bf96c2dbdd8ee45831b323c212dce7f00c189f
Nate Berkopec <nate.berkopec@gmail.com>
2020-04-16 14:43:57 +0900

    Change way we construct IAM instance

--- main.tf
+++ main.tf
@@ -12,14 +12,30 @@ data "template_file" "user_data" {
     template = "/scripts/user_data.ps1"
 }
 
-data "template_file" "iam-profile" {
-  template = "${file("assume-role-policy.json.tpl")}"
-
-  vars = {
-    resource = "${module.vpc.vpc_id}"
-  }
+resource "aws_iam_instance_profile" "parsec_profile" {
+  role = aws_iam_role.role.name
 }
 
+resource "aws_iam_role" "role" {
+  name = "assume_role"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
 
 # -- Cloud Posse SSH keypair Module -- #
 module "ssh_key_pair" {
